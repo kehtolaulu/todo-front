de3eed5acd6c002e642ff3a3c5e372fc980d35b9
Emilia Garipova <emilia270627@gmail.com>
2020-03-22 21:12:54 +0300

    Added login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,70 @@
 import React from 'react';
+import apiUrl from '../config';
 
 class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            error: false
+        };
+    }
+
+    handleUsernameChange = (e) => {
+        this.setState({ username: e.target.value });
+    }
+
+    handlePasswordChange = (e) => {
+        this.setState({ password: e.target.value });
+    }
+
+    authenticate = (e) => {
+        e.preventDefault();
+        let username = this.state.username.trim();
+        let password = this.state.password.trim();
+        fetch(apiUrl + "/login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username: username, password: password })
+        })
+            .then(response => {
+                if (!response.ok) {
+                    debugger
+                    this.setState({ error: true });
+                } else {
+                    // response.json();
+                    window.localStorage.jwt = response.token;
+                    this.props.history.push("/todos")
+                }
+            })
+    }
+
     render() {
-        return(
-            <div id="login-form">
-                <input type="text" name="username" placeholder="Username"></input>
-                <br></br>
-                <input type="password" name="password" placeholder="Password"></input>
-                <br></br>
-                <button id="loginButton">Login</button>
-            </div>
+        return (
+            <form id="login-form">
+                <input
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+                    value={this.state.username}
+                    onChange={this.handleUsernameChange}
+                />
+                <br />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={this.state.password}
+                    onChange={this.handlePasswordChange}
+                />
+                <br />
+                <button id="loginButton" onClick={this.authenticate}>Login</button>
+                <br />
+                <div id="error">{this.state.error && <p>Incorrect login or password</p>}</div>
+            </form>
         );
     }
 }

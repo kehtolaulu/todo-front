27b0c45560e7dda564d03865c9689b8038bd5198
Emilia Garipova <emilia270627@gmail.com>
2020-04-03 21:48:10 +0300

    Added routes for sign up

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import { Redirect } from "react-router-dom";
 import ToDoBox from './ToDoBox';
 import Login from './Login';
+import SignUp from './SignUp';
 import React from 'react';
 import apiUrl from '../config';
 
@@ -10,15 +11,14 @@ let todosPath = `${apiUrl}/todos`;
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <Router>
-                    <Switch>
-                        <LoginRoute exact path="/login" component={Login} />
-                        <Route exact path="/"> {<Redirect to="/todos" />} </Route>
-                        <PrivateRoute exact path="/todos" component={() => <ToDoBox url={todosPath} pollInterval={2000000} />} />
-                    </Switch>
-                </Router>
-            </div>
+            <Router>
+                <Switch>
+                    <LoginRoute exact path="/signup" component={SignUp} />
+                    <LoginRoute exact path="/login" component={Login} />
+                    <Route exact path="/"> {<Redirect to="/todos" />} </Route>
+                    <PrivateRoute exact path="/todos" component={() => <ToDoBox url={todosPath} pollInterval={2000000} />} />
+                </Switch>
+            </Router>
         );
     }
 }

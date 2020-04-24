6d1fd1246cebf9aacf52dfa9acb076cf625c1e16
Emilia Garipova <emilia270627@gmail.com>
2020-04-24 23:59:50 +0300

    Added redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import ReactDOM from 'react-dom';
-import App from './components/App'
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 import '../node_modules/materialize-css/dist/css/materialize.min.css';
 import '../node_modules/materialize-css/dist/js/materialize.min.js';
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('content')
 );
